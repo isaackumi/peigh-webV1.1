6acbae06dacedaef01c4e663fdccd368f3cc210b
isaackumi <isaac.kumi@ashesi.edu.gh>
2022-02-22 17:39:32 +0000

    Initial commit

---
+++ resources/data/ajax.php
@@ -0,0 +1,96 @@
+{
+"draw": 1,
+"recordsTotal": 57,
+"recordsFiltered": 57,
+"data": [
+[
+"Isaac Kumi",
+"isaackumi@gmail.com",
+"System Architect",
+"Edinburgh",
+"2011/04/25",
+"$320,800"
+],
+[
+"Emmanuel Nimo",
+"nimo@gmail.com",
+"Accountant",
+"Tokyo",
+"2011/07/25",
+"$170,750"
+],
+[
+"Ashton Cox",
+"ashton@example.com",
+"Junior Technical Author",
+"San Francisco",
+"2009/01/12",
+"$86,000"
+],
+[
+"Cedric Kelly",
+"cedric@example.com",
+"Senior Javascript Developer",
+"Edinburgh",
+"2012/03/29",
+"$433,060"
+],
+[
+"Airi Satou",
+"airi@example.com",
+"Accountant",
+"Tokyo",
+"2008/11/28",
+"$162,700"
+],
+[
+"Brielle Williamson",
+"brielle@example.com",
+"Integration Specialist",
+"New York",
+"2012/12/02",
+"$372,000"
+],
+[
+"Herrod Chandler",
+"herrod@example.com",
+"Sales Assistant",
+"San Francisco",
+"2012/08/06",
+"$137,500"
+],
+[
+"Rhona Davidson",
+"rhona@example.com",
+"Integration Specialist",
+"Tokyo",
+"2010/10/14",
+"$327,900"
+],
+[
+"Colleen Hurst",
+"colleen@example.com",
+"Javascript Developer",
+"San Francisco",
+"2009/09/15",
+"$205,500"
+],
+[
+"Sonya Frost",
+"sonya@example.com",
+"Software Engineer",
+"Edinburgh",
+"2008/12/13",
+"$103,600"
+],
+[
+"Jena Gaines",
+"jena@example.com",
+"Office Manager",
+"London",
+"2008/12/19",
+"$90,560"
+]
+
+]
+}
---
+++ resources/views/content/apps/todo/app-todo.blade.php
@@ -0,0 +1,77 @@
+
+@extends('layouts/contentLayoutMaster')
+
+@section('title', 'Workers')
+
+@section('vendor-style')
+  <!-- vendor css files -->
+
+  <link rel="stylesheet" href="{{asset('vendors/css/tables/datatable/dataTables.bootstrap5.min.css')}}">
+<link rel="stylesheet" href="{{asset('vendors/css/tables/datatable/extensions/dataTables.checkboxes.css')}}">
+<link rel="stylesheet" href="{{asset('vendors/css/tables/datatable/responsive.bootstrap5.min.css')}}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/editors/quill/katex.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/editors/quill/monokai-sublime.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/editors/quill/quill.snow.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/extensions/toastr.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/select/select2.min.css')) }}">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Inconsolata&family=Roboto+Slab&family=Slabo+27px&family=Sofia&family=Ubuntu+Mono&display=swap" rel="stylesheet">
+@endsection
+
+@section('page-style')
+  <!-- Page css files -->
+  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-quill-editor.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/extensions/ext-component-toastr.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('css/base/pages/app-email.css')) }}">
+  <link rel="stylesheet" href="{{asset('css/base/pages/app-invoice-list.css')}}">
+@endsection
+
+<!-- Sidebar Area -->
+@section('content-sidebar')
+{{-- @include('content/apps/email/app-email-sidebar') --}}
+@endsection
+
+@section('content')
+<section class="invoice-list-wrapper">
+  <div class="card">
+    <div class="card-datatable table-responsive">
+      <table class="invoice-list-table table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>#</th>
+            <th><i data-feather="trending-up"></i></th>
+            <th>Client</th>
+            <th>Total</th>
+            <th class="text-truncate">Issued Date</th>
+            <th>Balance</th>
+            <th>Invoice Status</th>
+            <th class="cell-fit">Actions</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </div>
+</section>
+@endsection
+
+@section('vendor-script')
+<!-- vendor js files -->
+  <script src="{{ asset(mix('vendors/js/editors/quill/katex.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/editors/quill/highlight.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/editors/quill/quill.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/extensions/toastr.min.js')) }}"></script>
+  <script src="{{ asset(mix('vendors/js/forms/select/select2.full.min.js')) }}"></script>
+  <script src="{{asset('vendors/js/extensions/moment.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/datatables.buttons.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/dataTables.bootstrap5.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/datatables.checkboxes.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/responsive.bootstrap5.js')}}"></script>
+@endsection
+@section('page-script')
+  <!-- Page js files -->
+  <script src="{{ asset(mix('js/scripts/pages/app-email.js')) }}"></script>
+  <script src="{{asset('js/scripts/pages/app-invoice-list.js')}}"></script>
+@endsection
